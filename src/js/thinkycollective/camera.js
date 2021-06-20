252f16ad15b8404cccd6965878912729e2634436
Joseph Mansfield <sftrabbit@gmail.com>
2021-06-20 21:31:16 +0100

    Pull camera between regions

--- src/js/thinkycollective/camera.js
+++ src/js/thinkycollective/camera.js
@@ -1,8 +1,12 @@
 var camera = null;
 var cameraTransition = null;
 var currentRegion = null;
+var currentRegionPrimary = null;
 
-function transitionCameraToRegion(region) {
+function transitionCameraToRegion(activeRegion) {
+  var region = activeRegion[0];
+  var isPrimary = activeRegion[1];
+
   cameraTransition = {
     start: (new Date()).getTime(),
     from: {
@@ -14,8 +18,62 @@ function transitionCameraToRegion(region) {
   };
 }
 
+function transitionCameraPulledByPlayer() {
+  var playerPositions = getPlayerPositions();
+
+  const playerPosition = {
+    x: (playerPositions[0]/(level.height))|0,
+    y: (playerPositions[0]%level.height)|0
+  };
+
+  var xDiff = Math.abs(playerPosition.x - currentRegion.cameraAnchor[0]);
+  var yDiff = Math.abs(playerPosition.y - currentRegion.cameraAnchor[1]);
+
+  var targetPosition = [];
+  if (xDiff >= yDiff) {
+    targetPosition = [
+      currentRegion.cameraAnchor[0] + (playerPosition.x - currentRegion.cameraAnchor[0]) * 0.4,
+      currentRegion.cameraAnchor[1]
+    ];
+  } else {
+    targetPosition = [
+      currentRegion.cameraAnchor[0],
+      currentRegion.cameraAnchor[1] + (playerPosition.y - currentRegion.cameraAnchor[1]) * 0.4
+    ];
+  }
+
+  cameraTransition = {
+    start: (new Date()).getTime(),
+    from: {
+      position: [camera.position[0], camera.position[1]]
+    },
+    to: {
+      position: targetPosition
+    }
+  };
+}
+
+function transitionCameraToPlayer() {
+  var playerPositions = getPlayerPositions();
+
+  const playerPosition = {
+    x: (playerPositions[0]/(level.height))|0,
+    y: (playerPositions[0]%level.height)|0
+  };
+
+  cameraTransition = {
+    start: (new Date()).getTime(),
+    from: {
+      position: [camera.position[0], camera.position[1]]
+    },
+    to: {
+      position: [playerPosition.x, playerPosition.y]
+    }
+  };
+}
+
 function initSmoothCamera() {
-  var region = getActiveRegion();
+  var region = getActiveRegion()[0];
 
   camera = {
     position: [region.cameraAnchor[0], region.cameraAnchor[1]]
